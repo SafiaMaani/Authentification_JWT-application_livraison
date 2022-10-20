43088273dd19a088780bdf25427d5bb089e45e61
SafiaMaani <saf.maani@gmail.com>
2022-10-20 09:29:23 +0100

    Connexion to DataBase

---
+++ Config/db.config.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const roles = ['client', 'livreur', 'manager']
+const Role = require('../Models/roleModel')
+
+const cnxDb = () => {
+    mongoose.connect(process.env.DB_URI).then((conn) => {
+        console.log("I'm connected to DB");
+        // Role.countDocuments({}, (err, count) => {
+        //     if (err) {
+        //         process.exit(1)
+        //     }
+        //     if (count == 0) {
+        //         roles.forEach(async (role) => {
+        //             const newRole = new Role({
+        //                 role
+        //             });
+        //             await newRole.save();
+        //         })
+        //     }
+        // })
+    }).catch(err => console.log(err))
+}
+
+module.exports = cnxDb
