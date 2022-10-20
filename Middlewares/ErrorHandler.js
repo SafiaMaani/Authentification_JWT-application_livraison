148de15695e9192a2da1ddbb6342e49cdc35dd36
SafiaMaani <saf.maani@gmail.com>
2022-10-20 09:31:31 +0100

    building middlewares + errorHandler function

---
+++ Middlewares/ErrorHandler.js
@@ -0,0 +1,11 @@
+function errorHandler(err, req, res, next) {
+  let message = err.message
+  let status = err.status
+  res.status(status).json({
+    error: true,
+    message,
+    status
+  })
+
+}
+module.exports = errorHandler
